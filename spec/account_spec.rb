e6277816886f0abe2df620a34ac56ba52bebf911
Felix Taljaard <felixtaljaard@hotmail.com>
2022-04-26 10:20:46 +0100

    Added a deposit method

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,4 +4,9 @@ describe Account do
   it 'has a starting balance of £0' do
     expect(subject.balance).to eq 0
   end
+
+  it 'can make a deposit' do
+    subject.deposit(100)
+    expect(subject.balance).to eq 100
+  end
 end
